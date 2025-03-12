ff91244927da0c94d7417560ec730bfedc3b2ad2
vikas <xanderbilla@gmail.com>
2025-03-12 12:31:48 +0530

    Project initialized

---
+++ src/main/java/com/kallan/clan/ClanApplication.java
@@ -0,0 +1,13 @@
+package com.kallan.clan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kallan/clan/ClanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kallan.clan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
