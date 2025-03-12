f0ca31e1c4ac45a4bd66760f9e5cefb4dd144a80
vikas <xanderbilla@gmail.com>
2025-03-13 03:31:55 +0530

    Updated blog Controller

--- src/main/java/com/kallan/clan/controller/BlogController.java
+++ src/main/java/com/kallan/clan/controller/BlogController.java
@@ -2,6 +2,7 @@ package com.kallan.clan.controller;
 
 import com.kallan.clan.response.ResponseWrapper;
 import com.kallan.clan.entity.Blog;
+import com.kallan.clan.service.AIService;
 import com.kallan.clan.service.BlogService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -17,30 +18,29 @@ import java.util.Optional;
 public class BlogController {
 
     private final BlogService blogService;
+    private final AIService aiService;
 
-    public BlogController(BlogService blogService) {
+    public BlogController(BlogService blogService, AIService aiService) {
         this.blogService = blogService;
+        this.aiService = aiService;
     }
 
     @PostMapping
     public ResponseEntity<ResponseWrapper<Blog>> createBlog(@Valid @RequestBody Blog blog) {
-        try {
+        return handleServiceCall(() -> {
             Blog createdBlog = blogService.createBlog(blog);
             return ResponseEntity.status(HttpStatus.CREATED)
-                    .body(ResponseWrapper.success("Blog has been created successfully.", createdBlog));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(ResponseWrapper.error("An error occurred while creating the blog: " + e.getMessage(),
-                            HttpStatus.INTERNAL_SERVER_ERROR.value()));
-        }
+                    .body(ResponseWrapper.success("Your blog has been created successfully!", createdBlog));
+        });
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ResponseWrapper<Blog>> getBlogById(@PathVariable Long id) {
-        Optional<Blog> blog = Optional.ofNullable(blogService.getBlogById(id));
-        return blog.map(value -> ResponseEntity.ok(ResponseWrapper.success("Blog found successfully.", value)))
-                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body(ResponseWrapper.error("No blog found with the provided ID: " + id,
+        Optional<Blog> blogOptional = Optional.ofNullable(blogService.getBlogById(id));
+        return blogOptional
+                .map(blog -> ResponseEntity.ok(ResponseWrapper.success("We found your blog!", blog)))
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body(ResponseWrapper.error("Sorry, we couldn't find a blog with the ID: " + id,
                                 HttpStatus.NOT_FOUND.value())));
     }
 
@@ -48,42 +48,64 @@ public class BlogController {
     public ResponseEntity<ResponseWrapper<Page<Blog>>> getAllBlogs(@RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
         Page<Blog> blogs = blogService.getAllBlogs(page, size);
-        return ResponseEntity.ok(ResponseWrapper.success("Blogs retrieved successfully.", blogs));
+        return ResponseEntity.ok(ResponseWrapper.success("Here are all the blogs!", blogs));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<ResponseWrapper<Blog>> updateBlog(@PathVariable Long id,
             @RequestBody Blog updatedBlog) {
-        try {
+        return handleServiceCall(() -> {
             Blog updated = blogService.updateBlog(id, updatedBlog);
             return Optional.ofNullable(updated)
-                    .map(blog -> ResponseEntity.ok(ResponseWrapper.success("Blog updated successfully.", blog)))
+                    .map(blog -> ResponseEntity
+                            .ok(ResponseWrapper.success("Your blog has been updated successfully!", blog)))
                     .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
-                            .body(ResponseWrapper.error("No blog found with the provided ID: " + id,
+                            .body(ResponseWrapper.error("Sorry, we couldn't find a blog with the ID: " + id,
                                     HttpStatus.NOT_FOUND.value())));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(ResponseWrapper.error("An error occurred while updating the blog: " + e.getMessage(),
-                            HttpStatus.INTERNAL_SERVER_ERROR.value()));
-        }
+        });
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<ResponseWrapper<String>> deleteBlog(@PathVariable Long id) {
-        try {
+        return handleServiceCall(() -> {
             boolean deleted = blogService.deleteBlog(id);
             if (deleted) {
                 return ResponseEntity.ok(
-                        ResponseWrapper.success("Blog has been deleted successfully.", "Deleted blog with ID: " + id));
+                        ResponseWrapper.success("Your blog has been deleted successfully!",
+                                "Deleted blog with ID: " + id));
             } else {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body(ResponseWrapper.error("No blog found with the provided ID: " + id,
+                        .body(ResponseWrapper.error("Sorry, we couldn't find a blog with the ID: " + id,
                                 HttpStatus.NOT_FOUND.value()));
             }
+        });
+    }
+
+    @GetMapping("/{id}/summary")
+    public ResponseEntity<ResponseWrapper<String>> getBlogSummary(@PathVariable Long id) {
+        Blog blog = blogService.getBlogById(id);
+        Optional<Blog> blogOptional = Optional.ofNullable(blog);
+        return blogOptional.map(b -> {
+            String summary = aiService.generateSummary(b.getContent());
+            return ResponseEntity.ok(ResponseWrapper.success("Here's the summary of your blog", summary));
+        })
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body(ResponseWrapper.error("Sorry, we couldn't find a blog with the ID: " + id,
+                                HttpStatus.NOT_FOUND.value())));
+    }
+
+    private <T> ResponseEntity<ResponseWrapper<T>> handleServiceCall(ServiceCall<T> serviceCall) {
+        try {
+            return serviceCall.execute();
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(ResponseWrapper.error("An error occurred while deleting the blog: " + e.getMessage(),
+                    .body(ResponseWrapper.error("Oops! Something went wrong: " + e.getMessage(),
                             HttpStatus.INTERNAL_SERVER_ERROR.value()));
         }
+    }
+
+    @FunctionalInterface
+    private interface ServiceCall<T> {
+        ResponseEntity<ResponseWrapper<T>> execute() throws Exception;
     }
 }
